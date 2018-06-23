fbe8282f40951b3487ebb888c142c1d66838659b
sam-pop <poplovitch.samuel@gmail.com>
2018-06-22 23:34:50 -0400

    added cli-table + print all items function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,25 @@ const Table = require('cli-table');
 const colors = require('colors');
 
 const DB = 'bamazon';
+const table = new Table({
+    chars: {
+        'top': '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        'bottom': '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        'left': '║',
+        'left-mid': '╟',
+        'mid': '─',
+        'mid-mid': '┼',
+        'right': '║',
+        'right-mid': '╢',
+        'middle': '│'
+    }
+});
 
 //config mysql db connection
 let connection = mysql.createConnection({
@@ -13,8 +32,27 @@ let connection = mysql.createConnection({
     database: DB
 });
 
+//establish a connection --> print all the items
 connection.connect(function (err) {
     if (err) throw err;
     console.log('Connection established! id: ' + connection.threadId + '\n');
-    // connection.end();
+    printAllItems();
 });
+
+
+//prints all the items in the DB with their info (table)
+function printAllItems() {
+    let query = connection.query('SELECT item_id, product_name, price FROM products', function (err, res) {
+        if (err) throw err;
+        table.push([colors.bgRed('Item ID'), colors.bgRed('Item Name'), colors.bgRed('Price')]);
+        for (let item of res) {
+            let items = [];
+            items.push(colors.gray(item.item_id));
+            items.push(colors.white(item.product_name));
+            items.push(colors.yellow(item.price));
+            table.push(items);
+        }
+        console.log(table.toString());
+    });
+    connection.end();
+}
