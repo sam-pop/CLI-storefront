22d3131e5bc7f598887b0870e616ece49b08b387
sam-pop <poplovitch.samuel@gmail.com>
2018-06-22 22:25:39 -0400

    init

--- schema.sql
+++ schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS bamazon;
 
 CREATE DATABASE bamazon;
 USE bamazon;
+
+CREATE TABLE products (
+item_id INT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price FLOAT(7,2) NOT NULL,
+stock_quantity INT default 0,
+PRIMARY KEY (item_id)
+);
