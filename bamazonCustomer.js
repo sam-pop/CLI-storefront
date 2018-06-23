bc0db61cc07668602b27a8e6e8f5b5de9b3fd856
sam-pop <poplovitch.samuel@gmail.com>
2018-06-22 23:04:52 -0400

    mysql database connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,3 +3,18 @@ const Table = require('cli-table');
 const colors = require('colors');
 
 const DB = 'bamazon';
+
+//config mysql db connection
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: DB
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('Connection established! id: ' + connection.threadId + '\n');
+    // connection.end();
+});
